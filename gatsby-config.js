ee84440686ae4c0ff91f0fc555dc36dfe8547572
Sveta Fo <svetafo@gmail.com>
2021-02-19 09:54:15 +0300

    colors + fail settings for YaCloud

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Simplefolio`,
+        name: `SF`,
         short_name: `Simplefolio`,
         start_url: `/`,
         background_color: `#fff`,
@@ -31,5 +31,11 @@ module.exports = {
         icon: 'src/images/favicon.png',
       },
     },
+    {
+      resolve: `gatsby-plugin-s3`,
+      options: {
+          bucketName: 'edibleclouds',
+      },
+  },
   ],
 };
